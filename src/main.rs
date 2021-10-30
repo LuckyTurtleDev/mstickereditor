45fc8fed4d8bb86d94e9c7052166af7f31a2dbf1
Lukas1818 <git@lukas1818.de>
2021-10-30 22:17:43 +0200

    get project_dirs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use adler::adler32_slice;
 use anyhow::anyhow;
 use anyhow::Context;
 use serde::Deserialize;
@@ -94,6 +93,8 @@ fn check_telegram_resp(mut resp: serde_json::Value) -> anyhow::Result<serde_json
 fn upload_to_matrix(sticker_image: &Vec<u8>) {
 	let image_checksum = adler::adler32_slice(&sticker_image);
 	println!("{}", image_checksum);
+	let project_dirs = directories::ProjectDirs::from("de","lukas1818","mstickereditor").unwrap();
+	println!("{}", project_dirs.data_dir().to_str().unwrap());
 }
 
 fn import(opt: OptImport) -> anyhow::Result<()> {
