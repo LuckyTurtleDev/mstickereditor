279359d6c0253ec166f33c28018a62b85fd1eddc
Dominic <git@msrd0.de>
2023-08-24 18:31:11 +0200

    tschüß hexe

--- mstickerlib/src/database/mod.rs
+++ mstickerlib/src/database/mod.rs
@@ -5,8 +5,8 @@ pub mod simple_file;
 
 pub type Hash = [u8; 64];
 
-///Database witch does store a map from a hash to a matrix media url,
-///to avaid duplicated uploads of the same file
+/// Database which stores mappings from hashes to matrix media urls,
+/// to avoid duplicat uploads of the same file.
 pub trait Database {
 	fn get(&self, hash: &Hash) -> Option<String>;
 	fn add(&self, hash: Hash, url: String) -> anyhow::Result<()>;
