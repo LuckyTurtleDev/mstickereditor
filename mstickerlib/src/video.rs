5ac2c06b326fb7de018835498dba2dd3c7ffd4a4
Dominic <git@msrd0.de>
2023-08-27 21:55:32 +0200

    there's 1000ms in a second

--- mstickerlib/src/video.rs
+++ mstickerlib/src/video.rs
@@ -35,8 +35,7 @@ pub(crate) fn webm2webp<P: AsRef<Path>>(file: &P) -> anyhow::Result<(WebPData, u
 	let mut encoder = Encoder::new((decoder.width(), decoder.height()))?;
 	let mut timestamp = 0;
 	let frame_rate = input.rate();
-	let time_per_frame = frame_rate.1 * 100 / frame_rate.0;
-	eprintln!("frame_rate={frame_rate}, time_per_frame={time_per_frame}");
+	let time_per_frame = frame_rate.1 * 1000 / frame_rate.0;
 	let mut receive_and_process_decoded_frames = |decoder: &mut decoder::Video| -> anyhow::Result<()> {
 		let mut decoded = Video::empty();
 		while decoder.receive_frame(&mut decoded).is_ok() {
